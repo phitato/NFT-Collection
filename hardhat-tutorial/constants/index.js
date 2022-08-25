3cea481ae7e1831ffaddc9acae456c572beba868
Phitato <arigotha@gmail.com>
2022-08-25 10:27:55 +0700

    NFT Collection Learn WEBDAO

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xBb374250223396828af6d3a65EcF1d117fC6d4CE";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
